b0e775c77280773a987f0916e750ef8e705c59c4
gyloff <31848467+gyloff@users.noreply.github.com>
2017-09-14 16:29:13 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.low;
+      this.high;
+      this.mid;
+    }
 
     setRange(min, max) {
-
+        this.low = min;
+        this.high = max;
     }
 
     guess() {
-
+     
+      return Match.ceil((this.high - this.low)/2);;
     }
 
     lower() {
-
+      this.high = this.mid;
     }
 
     greater() {
-
+      this.low = this.mid;
     }
 }
 
